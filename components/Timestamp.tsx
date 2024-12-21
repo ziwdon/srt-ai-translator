cbee9d690cec49ed84a17da53877fee20fa6f0ab
Yazin <yazin@amalinvest.com>
2024-12-21 21:28:23 +0300

    fix bug

--- components/Timestamp.tsx
+++ components/Timestamp.tsx
@@ -2,33 +2,48 @@ import React, { FC } from "react";
 import "tailwindcss/tailwind.css";
 import type { Chunk } from "@/types";
 
-const Timestamp: FC<Chunk> = ({ index, start, end, text }) => {
-  const formatTimestamp = (timestamp: string) => {
-    let [hours, minutes, secondsWithMs] = timestamp.split(":");
-    const [seconds, ms] = secondsWithMs.split(",");
+const Timestamp: FC<Chunk & { originalText?: string }> = ({
+	index,
+	start,
+	end,
+	text,
+	originalText,
+}) => {
+	const formatTimestamp = (timestamp: string) => {
+		let [hours, minutes, secondsWithMs] = timestamp.split(":");
+		const [seconds, ms] = secondsWithMs.split(",");
 
-    return `${minutes}:${seconds}.${ms[0]}`;
-  };
+		return `${minutes}:${seconds}.${ms[0]}`;
+	};
 
-  return (
-    <div className="flex">
-      <div className="flex flex-col items-center">
-        <div className="flex items-center mb-1">
-          <span className="text-xl">⏲</span>
-          <p className="ml-2 text-gray-400">{formatTimestamp(start)}</p>
-        </div>
-        <div className="flex items-center">
-          <span className="text-xl">⏲</span>
-          <p className="ml-2 text-gray-400">{formatTimestamp(end)}</p>
-        </div>
-      </div>
-      <textarea
-        className="flex-grow h-full ml-4 bg-gray-200 p-2 rounded-lg"
-        value={text}
-        readOnly
-      />
-    </div>
-  );
+	return (
+		<div className="flex">
+			<div className="flex flex-col items-center">
+				<div className="flex items-center mb-1">
+					<span className="text-xl">⏲</span>
+					<p className="ml-2 text-gray-400">{formatTimestamp(start)}</p>
+				</div>
+				<div className="flex items-center">
+					<span className="text-xl">⏲</span>
+					<p className="ml-2 text-gray-400">{formatTimestamp(end)}</p>
+				</div>
+			</div>
+			<div className="flex-grow flex gap-4 ml-4">
+				{originalText && (
+					<textarea
+						className="flex-grow h-full bg-gray-100 p-2 rounded-lg text-gray-500"
+						value={originalText}
+						readOnly
+					/>
+				)}
+				<textarea
+					className="flex-grow h-full bg-gray-200 p-2 rounded-lg"
+					value={text}
+					readOnly
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Timestamp;
